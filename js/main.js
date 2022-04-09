4bff8e606bc4354c2ffd07f52ea592d63e265d76
Arjun Yadav <ayadav@and.digital>
2022-04-09 17:57:57 +0100

    updating doll class

--- js/main.js
+++ js/main.js
@@ -1,35 +1,59 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-renderer.setClearColor( 0xb7c3f3, 1);
+renderer.setClearColor(0xb7c3f3, 1);
 
-const light = new THREE.AmbientLight( 0xffffff ); 
-scene.add( light );
+const light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
 
 camera.position.z = 5;
 
 const loader = new THREE.GLTFLoader();
 
-loader.load("../model/scene.gltf", function (gltf) {
-    scene.add( gltf.scene );
-    gltf.scene.scale.set(.4,.4,.4);
-    gltf.scene.position.set(0,-1,0);
-});
+class Doll {
+  constructor() {
+    loader.load('../model/scene.gltf', (gltf) => {
+      scene.add(gltf.scene);
+      gltf.scene.scale.set(0.4, 0.4, 0.4);
+      gltf.scene.position.set(0, -1, 0);
+      this.doll = gltf.scene;
+    });
+  }
+
+  lookBackward() {
+    gsap.to(this.doll.rotation, { y: -3.1415, duration: 0.45 });
+  }
+
+  lookForward() {
+    gsap.to(this.doll.rotation, { y: 0, duration: 0.45 });
+  }
+}
+
+let doll = new Doll();
+
+setTimeout(() => {
+  doll.lookBackward();
+}, 1000);
 
 function animate() {
-	renderer.render( scene, camera );
-	requestAnimationFrame( animate );
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
 }
 animate();
 
-window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener('resize', onWindowResize, false);
 
-function onWindowResize(){
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-};
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
